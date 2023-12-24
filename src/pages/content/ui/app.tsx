3de516b563f74d094c9acc30832c70a3dcc95e35
hehoku <hehoku@outlook.com>
2023-12-25 00:23:34 +0800

    feat: highlight selected text use span

--- src/pages/content/ui/app.tsx
+++ src/pages/content/ui/app.tsx
@@ -14,20 +14,40 @@ export default function App() {
 
   function highlightSelection() {
     const selection = window.getSelection();
-    // 如果没有选中文本，或者选中的文本为空，就不做任何事情
-    // 如果选中文本的父级元素已经有了高亮的样式，就不做任何事情
-    if (
-      !selection ||
-      selection.rangeCount === 0 ||
-      selection.anchorNode.parentElement.classList.contains('mr-mark-mr-mark')
-    )
-      return;
+    // 打印选中的内容
+    console.log(`selection: ${JSON.stringify(selection.toString())}`);
+    if (selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
 
-    const range = selection.getRangeAt(0);
-    const span = document.createElement('span');
-    span.className = 'mr-mark';
-    span.appendChild(range.extractContents());
-    range.insertNode(span);
+      // 创建一个新的 span 元素
+      const newNode = document.createElement('span');
+      newNode.className = 'mr-mark';
+
+      // 检查选择的范围是否在一个文本节点内
+      if (range.startContainer.nodeType === Node.TEXT_NODE && range.endContainer.nodeType === Node.TEXT_NODE) {
+        // 如果是，使用 surroundContents 方法
+        range.surroundContents(newNode);
+      } else {
+        // 如果不是，使用 extractContents 和 appendChild 方法
+        const fragment = range.extractContents();
+        console.log(`fragment: ${fragment}`);
+        newNode.appendChild(fragment);
+        range.insertNode(newNode);
+      }
+
+      // 如果 span 中有 p 元素，将p元素删除，保留 p 元素内的文本
+      if (newNode.querySelector('p')) {
+        const p = newNode.querySelector('p');
+        const text = p.innerText;
+        p.remove();
+        newNode.innerText = text;
+      }
+
+      // 清除选择
+      selection.removeAllRanges();
+    } else {
+      console.log('No range selected');
+    }
   }
 
   return <div className=""></div>;
